bfefbb07fa07ca7145f8c1c00f7416dd8922df5e
Michael Hatherly <michaelhatherly@gmail.com>
2016-11-04 00:28:59 +0200

    Refactor formats and colours
    Simplify format printing, implement 'shortnames' for tokens, and
    add `RGB` type to represent style colour rather than a hex string.

--- src/Format.jl
+++ src/Format.jl
@@ -1,25 +1,49 @@
 module Format
 
 import ..Highlights.Compiler: Context
-import ..Highlights.Themes: Theme, Style, has_fg, has_bg
+import ..Highlights.Themes: RGB, Style, Theme
 import ..Highlights.Tokens
+
+
+# RGB colours.
+
+render(io::IO, ::MIME"text/css", c::RGB) = (print(io, "rgb("); _tup(io, c); print(io, ")"))
+render(io::IO, ::MIME"text/latex", c::RGB) = (print(io, "[RGB]{"); _tup(io, c); print(io, "}"))
+
+_tup(io::IO, c::RGB) = print(io, Int(c.r), ",", Int(c.g), ",", Int(c.b))
 
 
 # Styles.
 
-function render(io::IO, ::MIME"text/css", style::Style)
-    has_fg(style)   && print(io, "color: #", style.fg, "; ")
-    has_bg(style)   && print(io, "background-color: #", style.bg, "; ")
+function render(io::IO, mime::MIME"text/css", style::Style)
+    if style.fg.active
+        print(io, "color: ")
+        render(io, mime, style.fg)
+        print(io, "; ")
+    end
+    if style.bg.active
+        print(io, "background-color: ")
+        render(io, mime, style.bg)
+        print(io, "; ")
+    end
     style.bold      && print(io, "font-weight: bold; ")
     style.italic    && print(io, "font-style: italic; ")
     style.underline && print(io, "text-decoration: underline; ")
     return nothing
 end
 
-function render(io::IO, ::MIME"text/latex", style::Style)
+function render(io::IO, mime::MIME"text/latex", style::Style)
     print(io, "[1]{")
-    has_fg(style)   && print(io, "\\textcolor[HTML]{", style.fg, "}{")
-    has_bg(style)   && print(io, "\\colorbox[HTML]{", style.bg, "}{")
+    if style.fg.active
+        print(io, "\\textcolor")
+        render(io, mime, style.fg)
+        print(io, "{")
+    end
+    if style.bg.active
+        print(io, "\\colorbox")
+        render(io, mime, style.bg)
+        print(io, "{")
+    end
     style.bold      && print(io, "\\textbf{")
     style.italic    && print(io, "\\textit{")
     style.underline && print(io, "\\underline{")
@@ -27,8 +51,8 @@ function render(io::IO, ::MIME"text/latex", style::Style)
     style.underline && print(io, "}")
     style.italic    && print(io, "}")
     style.bold      && print(io, "}")
-    has_bg(style)   && print(io, "}")
-    has_fg(style)   && print(io, "}")
+    style.bg.active && print(io, "}")
+    style.fg.active && print(io, "}")
     print(io, "}")
 end
 
@@ -48,7 +72,7 @@ function render(io::IO, mime::MIME"text/css", theme::Theme)
     render(io, mime, theme.base)
     println(io, "}")
     for (nth, style) in enumerate(theme.styles)
-        print(io, "pre.hljl > span.hljl-", Tokens.__TOKENS__[nth], " { ")
+        print(io, "pre.hljl > span.hljl-", Tokens.__SHORTNAMES__[nth], " { ")
         render(io, mime, style)
         println(io, "}")
     end
@@ -83,7 +107,7 @@ function render(io::IO, mime::MIME"text/latex", theme::Theme)
         """
     )
     for (nth, style) in enumerate(theme.styles)
-        print(io, "\\newcommand{\\HLJL", replace(string(Tokens.__TOKENS__[nth]), "_", ""), "}")
+        print(io, "\\newcommand{\\HLJL", Tokens.__SHORTNAMES__[nth], "}")
         render(io, mime, style)
         println(io)
     end
@@ -96,7 +120,7 @@ function render(io::IO, mime::MIME"text/html", ctx::Context, theme::Theme)
     println(io, "<pre class='hljl'>")
     for token in ctx.tokens
         print(io, "<span class='hljl-")
-        print(io, Tokens.__TOKENS__[token.value.value], "'>")
+        print(io, Tokens.__SHORTNAMES__[token.value.value], "'>")
         escape(io, mime, SubString(ctx.source, token.first, token.last))
         print(io, "</span>")
     end
@@ -109,13 +133,9 @@ function render(io::IO, mime::MIME"text/latex", ctx::Context, theme::Theme)
         if Tokens.__TOKENS__[token.value.value] === :TEXT
             print(io, SubString(ctx.source, token.first, token.last))
         else
-            for (nth, line) in enumerate(split(SubString(ctx.source, token.first, token.last), '\n'))
-                nth > 1 && println(io)
-                local name = replace(string(Tokens.__TOKENS__[token.value.value]), "_", "")
-                print(io, "(*@\\HLJL", name, "{")
-                escape(io, mime, line)
-                print(io, "}@*)")
-            end
+            print(io, "(*@\\HLJL", Tokens.__SHORTNAMES__[token.value.value], "{")
+            escape(io, mime, SubString(ctx.source, token.first, token.last))
+            print(io, "}@*)")
         end
     end
     println(io, "\n\\end{lstlisting}")
